ccb3bfaca9a0e68055d3b2b317f9186ed8c2be6c
Wutche <wuthchechikaome75@gmail.com>
2025-04-08 16:54:09 -0500

    PIP mode fixed and it's at the bottom right corner

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils'
 import NsfwOverlay from '../NsfwOverlay'
 import { useEffect, useRef } from 'react'
+// import CloseIcon from '../ui/closeicon'
 
 export default function VideoPlayer({
   src,
@@ -15,22 +16,25 @@ export default function VideoPlayer({
 }) {
   const videoRef = useRef<HTMLVideoElement>(null)
 
+  // Observe video in viewport
   useEffect(() => {
     const videoEl = videoRef.current
     if (!videoEl || !document.pictureInPictureEnabled) return
 
     const observer = new IntersectionObserver(
-      async ([entry]) => {
-        try {
-          if (!entry.isIntersecting && !document.pictureInPictureElement) {
-            if (!videoEl.paused) {
-              await videoEl.requestPictureInPicture()
-            }
-          } else if (entry.isIntersecting && document.pictureInPictureElement === videoEl) {
-            await document.exitPictureInPicture()
+      ([entry]) => {
+        const isCurrentPip = document.pictureInPictureElement === videoEl
+
+        if (!entry.isIntersecting) {
+          // Only trigger PiP if it's this video and not already in PiP
+          if (!videoEl.paused && !isCurrentPip) {
+            videoEl.requestPictureInPicture().catch(console.error)
           }
-        } catch (err) {
-          console.error('PIP error', err)
+        } else {
+          // If scrolled back and it's this video in PiP, close PiP
+          if (isCurrentPip) {
+            document.exitPictureInPicture().catch(console.error)
+          }
         }
       },
       { threshold: 0.5 }
@@ -38,21 +42,21 @@ export default function VideoPlayer({
 
     observer.observe(videoEl)
 
-    return () => {
-      observer.disconnect()
-    }
+    return () => observer.disconnect()
   }, [])
 
   return (
-    <div className="relative">
-      <video
-        ref={videoRef}
-        controls
-        className={cn('rounded-lg', size === 'small' ? 'h-[15vh]' : 'h-[30vh]', className)}
-        src={src}
-        onClick={(e) => e.stopPropagation()}
-      />
-      {isNsfw && <NsfwOverlay className="rounded-lg" />}
-    </div>
+    <>
+      <div className="relative">
+        <video
+          ref={videoRef}
+          controls
+          className={cn('rounded-lg', size === 'small' ? 'h-[15vh]' : 'h-[30vh]', className)}
+          src={src}
+          onClick={(e) => e.stopPropagation()}
+        />
+        {isNsfw && <NsfwOverlay className="rounded-lg" />}
+      </div>
+    </>
   )
 }
--- src/types.ts
+++ src/types.ts
@@ -115,3 +115,8 @@ export type TNip66RelayInfo = TRelayInfo & {
   relayType?: string
   countryCode?: string
 }
+
+export type VideoPlayerProps = {
+  src: string
+  className?: string
+}
